4d351e3d1569c1e80d81be01b5aafa4da8132993
Duong Nguyen <nhduong29@gmail.com>
2020-12-01 18:45:42 +0800

    s

---
+++ privacy-policy.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+  <title >Privacy Policy</title>
+
+
+  <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+<!-- color of address in mobile-->
+<meta name="theme-color" content="#2B401E" >
+<meta name="apple-mobile-web-app-status-bar-style" content="#2B401E">
+<!-- color of address in mobile-->
+
+<meta name="title" content="Privacy Policy">
+<meta name="description" content="By accessing this Website you accept all the terms and conditions of this Privacy Policy. We will require your consent for the use and disclosure of your personal data. By submitting your personal data to us online in our website, you agree to give us consent to use and disclose your personal data in accordance with this policy. If you do not agree with the terms and conditions of this Privacy Policy, you must immediately cease using and accessing this Website and any content, services or products contained in or available from this Website for which you need to provide personal information.">
+<meta name="robots" content="index, follow" />
+<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+
+<meta property="og:title" content="Privacy Policy"/>
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://www.oneprocurement.com/privacy-policy.php"/>
+<meta property="og:site_name" content="Privacy Policy"/>
+<meta property="og:description" content="By accessing this Website you accept all the terms and conditions of this Privacy Policy. We will require your consent for the use and disclosure of your personal data. By submitting your personal data to us online in our website, you agree to give us consent to use and disclose your personal data in accordance with this policy. If you do not agree with the terms and conditions of this Privacy Policy, you must immediately cease using and accessing this Website and any content, services or products contained in or available from this Website for which you need to provide personal information."/>
+
+  <!-- Bootstrap CDN -->
+
+  <link rel="stylesheet" href="bootstrap4/bootstrap4.min.css" >
+
+  <script  src="jquery/jquery-slim.min.js" ></script>
+  <script src="bootstrap4/popper.min.js" ></script>
+  <script src="bootstrap4/bootstrap4.min.js"></script>
+  <!-- Bootstrap CDN -->
+
+<!-- Google Fonts  -->
+<!-- <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet"> -->
+
+<!-- Google Fonts  -->
+
+<!-- favicon -->
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+<!-- favicon -->
+
+
+
+
+<!-- Css  -->
+<link rel="stylesheet" href="css/Style.css">
+<!-- Css  -->
+
+<style>
+@font-face { font-family: 'Source Sans Pro'; font-style: normal; font-weight: 400; src: url('/fonts/source-sans-pro-v14-latin-regular.eot'); src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url('/fonts/source-sans-pro-v14-latin-regular.eot?#iefix') format('embedded-opentype'), url('/fonts/source-sans-pro-v14-latin-regular.woff2') format('woff2'), url('/fonts/source-sans-pro-v14-latin-regular.woff') format('woff'), url('/fonts/source-sans-pro-v14-latin-regular.ttf') format('truetype'), url('../fonts/source-sans-pro-v14-latin-regular.svg#SourceSans Pro') format('svg'); }
+</style>
+
+</head>
+
+
+
+<body>
+
+<?php include("navbar.php"); ?>
+
+<!-- supplier hero -->
+<section class="policy-hero ">
+
+  <div class="container">
+
+
+    <div class="row pr-1 pl-1">
+      <div class="col-lg-10 col-12">
+        <h6> PRIVACY POLICY </h6>
+        <h1> OneProcurement Privacy Policy </h1>
+
+
+      </div>
+
+    </div>
+  </div>
+
+</section>
+<!-- supplier hero -->
+
+<!-- <the privacy policy>  -->
+
+<div class="container policy-text">
+<div class="row pr-1 pl-1">
+<div class="col-12">
+<b class="mb-2 d-block">PLEASE READ THIS PRIVACY POLICY CAREFULLY </b>
+<p>By accessing this Website you accept all the terms and conditions of this Privacy Policy. We will require your consent for the use and disclosure of your personal data. By submitting your personal data to us online in this Website, you agree to give us consent to use and disclose your personal data in accordance with this Privacy Policy. If you do not agree with the terms and conditions of this Privacy Policy, you must immediately cease using and accessing this Website and any content, services or products contained in or available from this Website for which you need to provide personal information.</p>
+</div>
+
+<div class="col-12">
+<b class="mb-4 d-block">Terms and Conditions</b>
+
+<div class="mb-4">
+<b class="d-block mb-3">1.	Scope of this Policy </b>
+<p>This Privacy Policy covers the collection, use and disclosure of personal information that is collected through or in connection with this OneProcurement website including all web pages related to the website but excluding any links to third party sites (the "Website").</p>
+</div>
+
+<div class="mb-4">
+<b class="d-block mb-3">2. Collection and Use of Personal Information </b>
+<p>Except as set out in this Privacy Policy, we will not disclose any personally identifiable information without your permission unless we are legally entitled or required to do so (for example, if required to do so by legal process or for the purposes of prevention of fraud or other crime) or if we believe that such action is necessary to protect and/or defend our rights, property or personal safety and those of our users/customers etc.</p>
+
+<p>We collect and use your personal information (including names, addresses, telephone numbers and email addresses) for the purposes of:</p>
+
+<ul>
+ <li>Responding to queries or requests submitted by you.</li>
+ <li>Administering or otherwise carrying out our obligations in relation to any agreement you have with us.</li>
+ <li>To improve the Website (we continually strive to improve Website offerings based on any information or feedback received from you). </li>
+ <li>Internal record keeping.</li>
+ <li>Informing you of any events, services and possible job opportunities.</li>
+ <li>Any other purposes for which you have provided the information.</li>
+ <li>Transmitting to any unaffiliated third parties including our third party service providers and agents, and relevant governmental and/or regulatory authorities, whether in Singapore or abroad, for the aforementioned purposes.</li>
+</ul>
+
+<p>If you do not wish us to use your personal information for these purposes, you may so advise us when you submit your personal information or at any time after that (see contact information below). Upon receiving your advice that you do not wish us to use your information for these purposes we will either not use it that way, or will discontinue such use immediately, as the case may be. </p>
+
+<p>We control the personal information collected through this Website. We may use third parties for the fulfilment of our obligations hereunder, and if so, we will require them to comply with the requirements of this Privacy Policy.</p>
+</div>
+
+<div class="mb-4">
+<b class="d-block mb-3">3. Access and Correction </b>
+<p>We may, upon your written request to us (see Section "Contact Us"), allow you to view or correct your personal data held by us, subject to the prohibitions and exceptions under the applicable Personal Data Protection laws.</p>
+</div>
+
+<div class="mb-4">
+<b class="d-block mb-3">4. Maintaining the accuracy of our records</b>
+<p>We aim to keep our information about you as accurate as possible. Please contact us to notify us of any changes to personal information you have provided to us that you would like to be made.</p>
+</div>
+
+<div class="mb-4">
+<b class="d-block mb-3">5. Security of your personal data</b>
+<p>We have implemented policies, procedures and technology to protecting your privacy from unauthorized access and improper use. These will be updated as new technology becomes available, as appropriate.</p>
+</div>
+
+<div class="mb-4">
+<b class="d-block mb-3">6. Retention</b>
+<p>We maintain and execute retention policies or records containing personal data for as long as it is necessary to fulfil the purpose for which it was collected, or as required by the relevant data protection laws of the country in which such personal data is collected. Different retention periods apply to the various kinds of personal data collected and held by us in accordance with policies in standing instructions and administration manuals.</p>
+</div>
+
+<div class="mb-4">
+<b class="d-block mb-3">7. Transfer of information overseas</b>
+<p>If we should transfer personal information outside of the country where the personal information originates from, we will only do so in compliance with the Personal Data Protection laws applicable in the country of origin.</p>
+<p>We will take all steps that are within its reasonable control to protect personal information no matter what country it is stored in or transferred to.</p>
+</div>
+
+<div class="mb-4">
+<b class="d-block mb-3">8. Personal Information Submitted to Third Party Websites</b>
+<p>Please note that once you leave this Website, even by using a link to or from this Website, this Privacy Policy no longer applies and we cannot be responsible for the privacy policies and practices of other websites. We recommend that you check the policy of each site you visit and contact the owner or operator of such website if you have any concerns or questions.</p>
+</div>
+
+<div class="mb-4">
+<b class="d-block mb-3">9. Collection and Use of Non-Personal Information</b>
+<p>We may also collect and use non-personal information provided to this Website, such as automatically collected information about the type of internet browsers you use or the website from which you linked to our Website, and aggregated information based on data that you have submitted to the Website (such as age, gender, and residency) from which you cannot be identified, for the purposes of assisting us in providing an effective service on this Website. We may from time to time supply third parties with this non-personal or aggregated data for uses in connection with this Website.
+</p>
+</div>
+
+<div class="mb-4">
+<b class="d-block mb-3">10. Cookies</b>
+<p>A cookie is a small file that is sent to your computer when you visit a website. When you visit the website again, the cookie allows that site to recognize your browser. Cookies may store user preferences and other information. You can reset your browser to refuse all cookies or to indicate when a cookie is being sent. We may store a cookie on your computer when you look at our Website. We are able to read these cookies for information purposes when you revisit our Website.</p>
+<p>The type of information collected from a cookie is specific to the PC used to access the Website, and includes the IP address, the date and time the PC visited the Website, what parts of our Website were looked at and whether the web pages requested were delivered successfully. This information is anonymous; it represents a computer rather than a person.</p>
+<p>A cookie in no way gives us access to your computer or any information about you, other than the data you choose to share with us. The cookie information may be used by us to improve our understanding about the use of the Website and to improve its operation and functionality.</p>
+<p>You can choose to accept or decline cookies. Most web browsers automatically accept cookies, but you can usually modify your browser setting to decline cookies if you prefer.</p>
+</div>
+
+<div class="mb-4">
+<b class="d-block mb-3">11. Changes to the Privacy Policy</b>
+<p>This Privacy Policy may change from time to time and if such changes substantially impact the collection, use and disclosure of your personal information, we will post those changes on this page in a manner that identifies that a change has taken place. Please check this Privacy Policy on a regular basis.</p>
+
+</div>
+
+<div class="mb-4">
+<b class="d-block mb-3">12. Contact Us</b>
+<p>If you have comments or questions about this Privacy Policy, you can reach us via the following link <a class="text-primary" href="contact-us.php"/><u>Contact Us</u></a>.</p>
+
+<b>This Privacy Policy was last updated on 30 November 2020.</b>
+
+</div>
+
+</div>
+
+
+</div>
+
+</div>
+
+<!-- <the privacy policy>  -->
+
+     <?php include("footer.php"); ?>
+
+<script>
+  $(function() {
+
+  $(document).ready(function () {
+    $(window).scroll(function () {
+      $('.navbar').toggleClass("scrolled", ($(window).scrollTop() > 20));
+       });
+  });
+  });
+
+</script>
+
+</body>
+
+</html>
